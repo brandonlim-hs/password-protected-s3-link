bf53725b36bf5194d7a2b6ffe9942d62d154f5cb
Brandon Lim <brandonlim.lim@gmail.com>
2020-07-25 21:09:36 +1000

    DynamoDB integration to get username and password

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-exports.handler = (event, context, callback) => {
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-10-08',
+  // If region is not specified, the region will be configured to the region of the requestor
+  // Which may be a different region to where the DynamoDB is reside
+  region: 'us-east-1',
+});
+
+exports.handler = async (event, context, callback) => {
 
   // Get request and request headers
   const request = event.Records[0].cf.request;
   const headers = request.headers;
 
   // Configure authentication
-  const authUser = 'USERNAME';
-  const authPass = 'PASSWORD';
+  let result = await dynamoDb.get({
+    TableName: 'REPLACE_ME_DYNAMODB_TABLE',
+    Key: {
+      path: 'report' + request.uri
+    }
+  }).promise();
+  let { username: authUser, password: authPass } = result['Item'] || {};
 
   // Construct the Basic Auth string
   const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
